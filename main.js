b965cd8718aed5b6cab12260132039716f5dbc17
njihun <102475695+njihun@users.noreply.github.com>
2024-05-17 17:48:58 +0900

    1.0.1
    디버그를 시작하면 자바스크립트가 중단된다는 점을 이용해 개발자 모드 접근 차단
    전체 화면이 아닌 경우 전체 화면 전환 가이드 노출

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ blocks = [
 let imgs = [];
 let loadedImgCount = 0;
 let guide = document.getElementById('guide');
+let guideClickEvent;
+/** 브라우저 창의 내부 사이즈 */
+let innerSize = [window.innerWidth, window.innerHeight];
+/** 브라우저 창의 전체 사이즈 */
+let outerSize = [window.outerWidth, window.outerHeight];
+/** 클라이언트가 사용하는 화면 전체 사이즈 */
+let screenSize = [window.screen.width, window.screen.height];
+let canvasSize = [0, 0];
 blocks.forEach((element, index) => {
     imgs[index] = new Image();
     imgs[index].src = element;
@@ -22,18 +30,23 @@ function imgLoaded() {
 function drawImage(params) {
     let canvas = document.createElement('canvas');
     let ratio = [20, 9]; //화면 비율 20:9
-    /** 브라우저 창의 내부 사이즈 */
-    let clientSize = [window.innerWidth, window.innerHeight];
-    /** 현재 사용되는 화면 사이즈 */
-    let screenSize = [window.screen.width, window.screen.height];
-    if(clientSize[0]<clientSize[1]){
+    innerSize = [window.innerWidth, window.innerHeight];
+    screenSize = [window.screen.width, window.screen.height];
+    // if(window.outerWidth-innerSize[0]>window.outerWidth/5) {
+    //     console.log(1);
+    //     setTimeout(()=>{
+    //         if(window.outerWidth-innerSize[0]>window.outerWidth/5)
+    //         window.close()
+    //     }, 500);
+    // }
+    if(innerSize[0]<innerSize[1]){
         //세로 비율 맞추는 용도
-        clientSize = [clientSize[1], clientSize[0]];
+        innerSize = [innerSize[1], innerSize[0]];
         screenSize = [screenSize[1], screenSize[0]];
     }
-    let canvasSize = [clientSize[0], clientSize[0]/ratio[0]*ratio[1]];
-    if(canvasSize[1] > clientSize[1]){
-        canvasSize = [clientSize[1]/ratio[1]*ratio[0], clientSize[1]];
+    canvasSize = [innerSize[0], innerSize[0]/ratio[0]*ratio[1]];
+    if(canvasSize[1] > innerSize[1]){
+        canvasSize = [innerSize[1]/ratio[1]*ratio[0], innerSize[1]];
     }
     canvas.width = canvasSize[0];
     canvas.height = canvasSize[1];
@@ -41,9 +54,11 @@ function drawImage(params) {
     let screen = document.getElementById('screen');
     if (canvas.getContext) {
         var ctx = canvas.getContext("2d");
-        if(clientSize[1] !== screenSize[1]){
+        if(innerSize[1] !== screenSize[1]){
             ctx.fillStyle = 'red';
-            // guideDraw('fullscreen');
+            guideDraw('fullscreen');
+        }else{
+            guide.style.display = 'none';
         }
         ctx.fillRect(canvasSize[0]/10, canvasSize[1]/2+canvasSize[0]/10*0.5, canvasSize[0]/10, canvasSize[0]/10);
         ctx.fillRect(canvasSize[0]/10, canvasSize[1]/2-canvasSize[0]/10*1.5, canvasSize[0]/10, canvasSize[0]/10);
@@ -56,26 +71,36 @@ function drawImage(params) {
 }
 
 function guideDraw(condition) {
-    guide.style.display = 'block';
     switch (condition) {
         case 'fullscreen':
-            
+            guide.style.fontSize = `${canvasSize[0]/100*3}px`;
+            document.querySelector('#guide > *:nth-child(2)').style.marginTop = `${canvasSize[1]/100*10}px`;
+            if(guideClickEvent!=='fullscreen'){
+                guideClickEvent = 'fullscreen';
+                document.querySelector('#guide > *:nth-child(1)').textContent = '아래 버튼을 눌러 전체화면으로 전환하세요.';
+                document.querySelector('#guide > *:nth-child(2)').textContent = '전체 화면';
+                document.querySelector('#guide > *:nth-child(2)').onclick = () => {
+                    fullscreen();
+                    console.log('fullscreen', innerSize[0], screenSize[0]);
+                };
+            }
             break;
     
         default:
             break;
     }
+    guide.style.display = 'flex';
     
 }
 
-const fullscreen = element => {
+function fullscreen(element = document.documentElement) {
     if (element.requestFullscreen) return element.requestFullscreen();
     if (element.webkitRequestFullscreen) return element.webkitRequestFullscreen();
     if (element.mozRequestFullScreen) return element.mozRequestFullScreen();
     if (element.msRequestFullscreen) return element.msRequestFullscreen();
 }
 
-const exitFullScreen = () => {
+function exitFullScreen() {
     if (document.exitFullscreen) return document.exitFullscreen();
     if (document.webkitCancelFullscreen) return document.webkitCancelFullscreen();
     if (document.mozCancelFullScreen) return document.mozCancelFullScreen();
